c83296852664da6cbce75918016f2592db6d3472
Ignacio Guerendiain <iguerendiain@gmail.com>
2024-08-07 12:41:59 -0300

    Home screen UI

--- app/src/main/java/iguerendiain/vamacodingchallenge/ui/home/HomeScreen.kt
+++ app/src/main/java/iguerendiain/vamacodingchallenge/ui/home/HomeScreen.kt
@@ -3,12 +3,10 @@ package iguerendiain.vamacodingchallenge.ui.home
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -18,8 +16,6 @@ import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
