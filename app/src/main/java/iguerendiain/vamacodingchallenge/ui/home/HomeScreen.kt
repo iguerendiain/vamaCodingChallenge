c42a297c824314436d581983cead2f3e54ab4cb0
Ignacio Guerendiain <iguerendiain@gmail.com>
2024-08-07 12:40:40 -0300

    Home screen UI

--- app/src/main/java/iguerendiain/vamacodingchallenge/ui/home/HomeScreen.kt
+++ app/src/main/java/iguerendiain/vamacodingchallenge/ui/home/HomeScreen.kt
@@ -1,14 +1,38 @@
 package iguerendiain.vamacodingchallenge.ui.home
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import de.palm.composestateevents.EventEffect
+import iguerendiain.vamacodingchallenge.BuildConfig
+import iguerendiain.vamacodingchallenge.R
 import iguerendiain.vamacodingchallenge.ui.vm.MainViewModel
 import iguerendiain.vamacodingchallenge.ui.navigation.Screen
+import iguerendiain.vamacodingchallenge.ui.vm.MainState
 
 @Composable
 fun HomeScreen(navController: NavController, viewModel: MainViewModel) {
@@ -21,20 +45,92 @@ fun HomeScreen(navController: NavController, viewModel: MainViewModel) {
         navController.navigate(Screen.AlbumDetailsScreen.route + "/${it.id}")
     }
 
-    Column {
-        Row{
-            Text(text = "HOME PAGE")
-            Button(onClick = { viewModel.refreshAlbums() }){
-                Text(text = "FORCE REFRESH")
+    if (mainState.albums.isEmpty()) {
+        when (mainState.loadingState) {
+            MainState.STATE_IDLE -> {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(all = 20.dp),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        textAlign = TextAlign.Center,
+                        color = MaterialTheme.colorScheme.secondary,
+                        fontSize = 24.sp,
+                        lineHeight = 42.sp,
+                        text = stringResource(id = R.string.home_noalbums)
+                    )
+                    TextButton(onClick = { viewModel.refreshAlbums() }) {
+                        Text(
+                            textAlign = TextAlign.Center,
+                            color = MaterialTheme.colorScheme.primary,
+                            fontSize = 32.sp,
+                            text = stringResource(id = R.string.home_noalbums_try_download)
+                        )
+                    }
+                }
             }
-            Button(onClick = { viewModel.clearDB() }){
-                Text(text = "CLEAR DB")
+            MainState.STATE_LOADING -> {
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    CircularProgressIndicator(modifier = Modifier.size(120.dp))
+                }
             }
         }
-        AlbumList(
-            albums = viewModel.state.value.albums,
-            onAlbumSelected = {viewModel.selectAlbum(it) }
-        )
+    } else {
+        Column {
+            Row(modifier = Modifier.height(56.dp)) {
+                Text(
+                    fontWeight = FontWeight.Black,
+                    fontSize = 24.sp,
+                    modifier = Modifier
+                        .padding(
+                            horizontal = 20.dp,
+                            vertical = 12.dp
+                        )
+                        .weight(1f),
+                    text = stringResource(
+                        id = R.string.home_title,
+                        BuildConfig.ALBUM_DOWNLOAD_LIMIT,
+                        BuildConfig.ALBUM_DOWNLOAD_COUNTRY
+                    )
+                )
+
+                if (BuildConfig.DEBUG) {
+                    IconButton(onClick = { viewModel.refreshAlbums() }) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_refresh),
+                            contentDescription = null,
+                            tint = MaterialTheme.colorScheme.primary
+                        )
+                    }
+
+                    IconButton(onClick = { viewModel.clearDB() }) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_delete),
+                            contentDescription = null,
+                            tint = MaterialTheme.colorScheme.primary
+                        )
+                    }
+                }
+
+                if (mainState.loadingState == MainState.STATE_LOADING)
+                    CircularProgressIndicator(
+                        modifier = Modifier
+                            .size(48.dp)
+                            .padding(all = 4.dp),
+                    )
+            }
+            AlbumList(
+                albums = mainState.albums,
+                onAlbumSelected = { viewModel.selectAlbum(it) }
+            )
+        }
     }
 
     mainState.albumDownloadErrorEvent?.let { AlbumDownloadErrorDialog(
