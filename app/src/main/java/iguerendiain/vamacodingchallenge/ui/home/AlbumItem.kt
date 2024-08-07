e6968010a8d9f73fee8d9e9740822f57cc14fdb9
Ignacio Guerendiain <iguerendiain@gmail.com>
2024-08-07 13:09:47 -0300

    AlbumList UI

--- app/src/main/java/iguerendiain/vamacodingchallenge/ui/home/AlbumItem.kt
+++ app/src/main/java/iguerendiain/vamacodingchallenge/ui/home/AlbumItem.kt
@@ -1,28 +1,78 @@
 package iguerendiain.vamacodingchallenge.ui.home
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import iguerendiain.vamacodingchallenge.model.Album
 
 @Composable
 fun AlbumItem(album: Album, onClick: () -> Unit){
-    var imageWidth by remember { mutableIntStateOf(0) }
+    var cellWidth by remember { mutableIntStateOf(0) }
 
-    Column(
+    Box(
+        contentAlignment = Alignment.BottomStart,
         modifier = Modifier
-            .onGloballyPositioned { imageWidth = it.size.width }
+            .onGloballyPositioned { cellWidth = it.size.width }
+            .padding(all = 5.dp)
+            .aspectRatio(1f)
+            .clip(RoundedCornerShape(8.dp))
             .clickable { onClick.invoke() }
-    ) {
-        AsyncImage(model = album.getArtworkURL(imageWidth), contentDescription = null)
-        Text(text = "${album.name} -- ${album.artistName}")
+    ){
+        AsyncImage(
+            model = album.getArtworkURL(cellWidth),
+            contentDescription = null
+        )
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(
+                    Brush.verticalGradient(
+                        listOf(Color.Transparent, Color.Black)
+                    )
+                )
+        )
+        Column(
+            modifier = Modifier
+                .padding(horizontal = 8.dp)
+        ) {
+            Text(
+                text = album.name,
+                fontWeight = FontWeight.Bold,
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis,
+                color = MaterialTheme.colorScheme.primary,
+                fontSize = 16.sp
+            )
+            Text(
+                text = album.artistName,
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis,
+                color = MaterialTheme.colorScheme.secondary,
+                fontSize = 11.sp
+            )
+        }
     }
 }
--- app/src/main/java/iguerendiain/vamacodingchallenge/ui/home/AlbumList.kt
+++ app/src/main/java/iguerendiain/vamacodingchallenge/ui/home/AlbumList.kt
@@ -1,10 +1,12 @@
 package iguerendiain.vamacodingchallenge.ui.home
 
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import iguerendiain.vamacodingchallenge.model.Album
 
 @Composable
@@ -14,7 +16,9 @@ fun AlbumList(
 ) {
     LazyVerticalGrid(
         columns = GridCells.Fixed(2),
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 5.dp)
     ) {
         items(albums.size) {
             val album = albums[it]
